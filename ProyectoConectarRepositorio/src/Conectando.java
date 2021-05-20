9339ebfc34fbeae9afa88624f51f546ab7104e1e
mint <mint@mint-VirtualBox>
2021-05-20 09:19:15 +0200

    Version 1 del proyecto

---
+++ ProyectoConectarRepositorio/src/Conectando.java
@@ -0,0 +1,12 @@
+
+/**
+ * 
+ * @author Noureddine El Qaddoury
+ * @version 1.0.0
+ *
+ */
+public class Conectando {
+       public static void main(String[] args) {
+		System.out.println("Esto es una aplicacion para gestionar pacientes covid");
+	}                                                                          
+}
